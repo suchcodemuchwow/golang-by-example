906a1836683a01468ecf75f912f13e2f7870c63a
baturalp <baturalp@instabox.se>
2021-05-02 20:47:32 +0200

    feat: 🎸 channels.go

---
+++ channels.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	messages := make(chan string)
+
+	go func() { messages <- "ping" }()
+	go func() { messages <- "pong" }()
+
+	msg := <-messages
+
+	fmt.Println(msg)
+}
